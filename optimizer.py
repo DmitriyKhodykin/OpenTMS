1e2215b35b15c2d49d2eb46c1fbcc63c3e8b9324
DmitriyKhodykin <d.hodikin@yandex.ru>
2021-04-13 17:09:59 +0300

    Рефакторинг кода оптимизационного модуля

--- optimizer.py
+++ optimizer.py
@@ -4,10 +4,10 @@
 методами.
 """
 
-import numpy as np
+import sys
 import pandas as pd
 import six
-import sys
+
 sys.modules['sklearn.externals.six'] = six
 import mlrose
 from geocoding import Geocoding
@@ -17,17 +17,18 @@ from auth import auth
 class Optimizer:
     """Решает оптимизационную транспортную задачу.
     """
-    def __init__(self, coords_list):
-        self.coords_list = coords_list
+
+    def __init__(self, geo_points):
+        self.geo_points = geo_points
 
     def get_map_route(self):
         """Возвращает лучший путь обхода точек, заданных гео-координатами.
         """
-        fitness_coords = mlrose.TravellingSales(coords = self.coords_list)
+        fitness_coordinates = mlrose.TravellingSales(coords=self.geo_points)
 
         problem_fit = mlrose.TSPOpt(
-            length = len(coords_list),
-            fitness_fn = fitness_coords,
+            length=len(self.geo_points),
+            fitness_fn=fitness_coordinates,
             maximize=False
         )
 
@@ -37,52 +38,44 @@ class Optimizer:
             mutation_prob=0.2,
             max_attempts=100,
             max_iters=10,
-            random_state = 2
+            random_state=2
         )
 
         return best_state
 
 
-order = pd.DataFrame(
-    {
-        'city': [
-            'Россия, Воронеж',
-            'Россия, Саратов',
-            'Россия, Москва',
-            'Россия, Рязань',
-            'Росиия, Казань'
-        ]
-    }
-)
-
 if __name__ == "__main__":
+
+    ORDER = pd.DataFrame(
+        {
+            'city': [
+                'Россия, Воронеж',
+                'Россия, Саратов',
+                'Россия, Москва',
+                'Россия, Рязань',
+                'Росиия, Казань'
+            ]
+        }
+    )
 
     # Регистрационные данные для сервиса
     MAPBOX_TOKEN: str = auth.mapbox_token
 
-    # Определение расстояния между точками
-    dist_list: list = []
-    coords_list: list = []
+    # Определение координат точек
+    coordinates_list: list = []
     gc = Geocoding(MAPBOX_TOKEN)
 
-    for index_a, row_a in order.iterrows():
-        for index_b, row_b in order.iterrows():
-            if index_a != index_b:
-                distance: int = gc.distance_mapbox(row_a, row_b)
-                pairs: tuple = (index_a, index_b, distance)
-                coords: tuple = (
-                    gc.get_coordinates(row_a),
-                    gc.get_coordinates(row_b)
-                )
-                dist_list.append(pairs)
-                coords_list.append(coords)
+    for index, row in ORDER.iterrows():
+        lat_lng = gc.get_coordinates(str(row))
+        coordinates: tuple = (
+            lat_lng[0],
+            lat_lng[1]
+        )
 
-    print(dist_list)
-    print(coords_list)
+        coordinates_list.append(coordinates)
 
-    fitness_dists = mlrose.TravellingSales(distances = dist_list)
-    problem_fit = mlrose.TSPOpt(length = 8, fitness_fn = fitness_dists, maximize=False)
-    best_state, best_fitness = mlrose.genetic_alg(problem_fit, mutation_prob = 0.2,
-                                              max_attempts = 100, random_state = 2)
-    print(best_state)
-    print(best_fitness)
+    print(coordinates_list)
+
+    order_opt = Optimizer(coordinates_list)
+
+    print(order_opt.get_map_route())
