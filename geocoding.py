9644c447d7bcc9aec88176c66e563d8cacb2eb8f
Dmitriy_Khodykin <d.hodikin@yandex.ru>
2021-04-30 09:37:39 +0300

    Geocoding service is changed

--- geocoding.py
+++ geocoding.py
@@ -1,13 +1,11 @@
-"""Модуль геокодирования.
+"""Geocoding module.
 
-Прямая задача геокодирования - получение координат (широта и долгота)
-географической точки по ее наименованию. Для решения этой задачи используется
-сервис: https://www.mapbox.com/
+The task of direct geocoding is to obtain coordinates (latitude and longitude)
+of a geographic point by its name.
 
-После регистрации в сервисе (в личном кабинете) доступен token, который
-является обязательным для передачи во всех запросах к сервису.
+To solve this problem, the service is used: DaData.ru
 
-Описание API для доступа к сервису: https://docs.mapbox.com/api/overview/
+API docs: https://dadata.ru/api/geocode/
 """
 
 import json             # Работа с JSON
@@ -16,14 +14,16 @@ from auth import auth   # Регистрационные данные
 
 
 def geocoding(address: str) -> list:
-    """Возвращает координаты точки (широта, долгота), например,
-    [38.05, 41.06]
+    """Latitude and longitude returns [0], and Kladr 25-symbol street-code, e.g:
+    [38.05, 41.06, 3600000100006660000000000].
     """
-
     url = "https://cleaner.dadata.ru/api/v1/clean/address"
 
-    payload = address
+    address = f'[ "{address}" ]'
 
+    payload = address.encode('utf-8')
+
+    # Request params
     headers = {
         'authorization': auth.da_token,
         'x-secret': auth.da_secret,
@@ -33,22 +33,30 @@ def geocoding(address: str) -> list:
 
     request = requests.request("POST", url, data=payload, headers=headers)
 
+    # Service response
     response = json.loads(request.text, encoding='utf-8')
-
-    print(response)
-
     geo_point = response[0]
-    lat = geo_point["geo_lat"]
-    lng = geo_point["geo_lon"]
 
-    return [lat, lng]
+    # Latitude and Longitude
+    lat = float(geo_point["geo_lat"])
+    lon = float(geo_point["geo_lon"])
+
+    #Kladr street-code
+    kladr = geo_point["street_kladr_id"]
+    if len(kladr) != 25:
+        delta = 25 - len(kladr)  # 25-symbol is needed
+        kladr_appended = int(kladr + ('0' * delta))
+    else:
+        kladr_appended = kladr
+
+    return [lat, lon, kladr_appended]
 
 
 if __name__ == "__main__":
 
-    # Наименования геоточки
-    ADDRESS = '[ "воронеж патриотов 11" ]'
+    # Geopoint name
+    ADDRESS = "воронеж патриотов 11"
 
     # Определение координат
     coordinates = geocoding(ADDRESS)
-    print('Координаты точки:', coordinates)
+    print('Result:', coordinates)
