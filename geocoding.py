94a6fa651f5e7a9dde443b3d4159dea31d9e124f
Dmitriy_Khodykin <d.hodikin@yandex.ru>
2021-04-30 10:42:10 +0300

    TryExcept loop is done

--- geocoding.py
+++ geocoding.py
@@ -34,29 +34,41 @@ def geocoding(address: str) -> list:
     request = requests.request("POST", url, data=payload, headers=headers)
 
     # Service response
-    response = json.loads(request.text, encoding='utf-8')
-    geo_point = response[0]
+    if request.status_code == 200:
+        try:
+            response = json.loads(request.text, encoding='utf-8')
+        except json.decoder.JSONDecodeError:
+            response = None
+            print('error: JSONDecoding received unexpected value')
 
-    # Latitude and Longitude
-    lat = float(geo_point["geo_lat"])
-    lon = float(geo_point["geo_lon"])
+        try:
+            geo_point = response[0]
 
-    #Kladr street-code
-    kladr = geo_point["street_kladr_id"]
-    if len(kladr) != 25:
-        delta = 25 - len(kladr)  # 25-symbol is needed
-        kladr_appended = int(kladr + ('0' * delta))
+            # Latitude and Longitude
+            lat = float(geo_point["geo_lat"])
+            lon = float(geo_point["geo_lon"])
+
+            # Kladr street-code
+            kladr = geo_point["street_kladr_id"]
+            if len(kladr) != 25:
+                delta = 25 - len(kladr)  # 25-symbol is needed
+                kladr_appended = int(kladr + ('0' * delta))
+            else:
+                kladr_appended = kladr
+
+            return [lat, lon, kladr_appended]
+
+        except (TypeError, IndexError, KeyError):
+            print('error Index or Key: Address not found or does not exist')
     else:
-        kladr_appended = kladr
-
-    return [lat, lon, kladr_appended]
+        print('error: Request code != 200')
 
 
 if __name__ == "__main__":
 
     # Geopoint name
-    ADDRESS = "воронеж патриотов 11"
+    ADDRESS = "воронеж труда 11"
 
-    # Определение координат
+    # Result: [51.6813317, 39.1823317, 3600000100009020000000000]
     coordinates = geocoding(ADDRESS)
     print('Result:', coordinates)
