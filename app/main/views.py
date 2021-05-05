a9facd2804958caf05c3b553f41bbfc3fcb60062
Dmitriy_Khodykin <d.hodikin@yandex.ru>
2021-05-05 17:02:11 +0300

    Templates html added

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,9 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("<h4>Hi There!</h4>")
+    return render(request, 'main/index.html')
 
 
 def about(request):
