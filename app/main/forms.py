6f4c2aaf0a7e6cf5057f9da621413eb440fa89ad
Dmitriy_Khodykin <d.hodikin@yandex.ru>
2021-05-17 16:36:34 +0300

    Forms and views are bounded

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,10 +1,23 @@
 from .models import Orders
-from django.forms import ModelForm
+from django.forms import ModelForm, TextInput
 
 
 class OrderForm(ModelForm):
     class Meta:
         model = Orders
         fields = ["address", "cargo", "weight"]
-
-
+        widgets = {
+            "address": TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Воронеж Патриотов 20',
+                'value': '""'
+            }),
+            "cargo": TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Пищевые ингредиенты'
+            }),
+            "weight": TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': '900'
+            })
+        }
--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Route
+from .forms import OrderForm
 
 
 def index(request):
-    return render(request, 'main/index.html')
+
+    error = ''
+
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('result')
+        else:
+            error = 'Проверьте корректность заполнения формы'
+
+    form = OrderForm()
+
+    context = {
+        'form': form,
+        'error': error
+    }
+
+    return render(request, 'main/index.html', context)
 
 
 def signin(request):
